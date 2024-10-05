59337e0027b7d3f44df7e8d93a7bf00911c29477
Dinesh R Thangavel <rtdinesh.89@gmail.com>
2024-10-05 14:00:18 -0700

    Fixing script to create cache dir during build

--- scripts/generate-post-data.js
+++ scripts/generate-post-data.js
@@ -1,4 +1,5 @@
-const fs = require('fs');
+//const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 const matter = require('gray-matter');
 const slugify = require('slugify'); // You'll need to install this package
@@ -7,12 +8,14 @@ const postsDirectory = path.join(process.cwd(), 'posts');
 const postsOutputFile = path.join(process.cwd(), 'cache/posts-data.json');
 const tagsOutputFile = path.join(process.cwd(), 'cache/tags-data.json');
 
-function generatePostsData() {
-  const fileNames = fs.readdirSync(postsDirectory);
-  const allPostsData = fileNames.map(fileName => {
+async function generatePostsData() {
+  await fs.mkdir(path.dirname(postsOutputFile), { recursive: true });
+
+  const fileNames = await fs.readdir(postsDirectory);
+  const allPostsData = await Promise.all(fileNames.map(async (fileName) => {
     const slug = fileName.replace(/\.mdx?$/, '');
     const fullPath = path.join(postsDirectory, fileName);
-    const fileContents = fs.readFileSync(fullPath, 'utf8');
+    const fileContents = await fs.readFile(fullPath, 'utf8');
     const { data, content } = matter(fileContents)
 
     // Create a slug for each tag
@@ -25,9 +28,12 @@ function generatePostsData() {
       ...data,
       content
     };
-  }).sort((a, b) => new Date(b.date) - new Date(a.date));
+  }));
+  
+  allPostsData.sort((a, b) => new Date(b.date) - new Date(a.date));
 
-  fs.writeFileSync(postsOutputFile, JSON.stringify(allPostsData));
+
+  await fs.writeFile(postsOutputFile, JSON.stringify(allPostsData));
 
   // Generate tags data
   const tagData = {};
@@ -48,7 +54,7 @@ function generatePostsData() {
     .filter(([_, data]) => data.count > 0)
     .map(([slug, data]) => ({ slug, name: data.name }));
 
-  fs.writeFileSync(tagsOutputFile, JSON.stringify(validTags));
+  await fs.writeFile(tagsOutputFile, JSON.stringify(validTags));
 
   console.log('Posts and tags data generated successfully.');
 }
